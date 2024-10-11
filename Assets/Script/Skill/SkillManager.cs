f3e77fd439fb25ef99aab179ffe32c80d87f68f3
HyeongSeok2454 <gudtjr2454@g.shingu.ac.kr>
2024-10-11 10:48:29 +0900

    [Feat] 제너릭 까지 진행

---
+++ Assets/Script/Skill/SkillManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillManager : MonoBehaviour
+{
+    public Player TargetPlayer;
+    public Enemy TargetEnemy;
+    [SerializeField]
+    public List<Enemy> targets = new List<Enemy>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    } 
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyUp(KeyCode.Alpha1))
+        {
+            var fireball = new Skill<ISkillTarget, DamageEffect>("Fireball", new DamageEffect(20));
+            fireball.Use(TargetEnemy);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Alpha2))
+        {
+            var healSpell = new Skill<Player, HealEffect>("Heal", new HealEffect(20));
+            healSpell.Use(TargetPlayer);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Alpha3))
+        {
+            var multiTargetSkill = new Skill<ISkillTarget, DamageEffect>("AoE Attack", new DamageEffect(10));
+            foreach(var target in targets)
+            {
+                multiTargetSkill.Use(target);
+            }
+        }
+    }
+}
